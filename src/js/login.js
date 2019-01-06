698aa60218246e2264abb31fdf7bab597fc6c60f
kuang920 <282682091@qq.com>
2019-01-07 00:34:39 +0800

    feat register login 静态页加载头尾

---
+++ src/js/login.js
@@ -0,0 +1,7 @@
+//login业务逻辑
+require(["requirejs.config"],()=>{
+    //引入login需要的配置文件
+    require(["jquery","header","footer"],()=>{
+        
+    })
+})
---
+++ src/js/register.js
@@ -0,0 +1,7 @@
+//注册业务逻辑
+require(["./requirejs.config"],()=>{
+    //引入register需要的模块
+    require(["jquery","header","footer"],()=>{
+        
+    })
+})
